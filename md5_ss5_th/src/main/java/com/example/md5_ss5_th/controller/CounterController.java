66494f5588ad0540e91d76194323d8f995ea19a1
Lê Thế Thuỳ <you@example.com>
2023-09-11 07:50:04 +0700

    done

---
+++ md5_ss5_th/src/main/java/com/example/md5_ss5_th/controller/CounterController.java
@@ -0,0 +1,22 @@
+package com.example.md5_ss5_th.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import com.example.md5_ss5_th.model.Counter;
+@Controller
+@SessionAttributes("counter")
+public class CounterController {
+    @ModelAttribute("counter")
+    public Counter setUpCounter() {
+        return new Counter();
+    }
+
+    @GetMapping("/")
+    public String get(@ModelAttribute("counter") Counter counter) {
+        counter.increment();
+        return "/index";
+    }
+
+}
